85ee844eb06bd7dedbf90f46466a14932c67ae28
José Miguel Cordero Carvacho <josemcorderoc@gmail.com>
2024-11-16 21:35:34 +0000

    refactor: generate folium with numerical values directly in map generator

--- prompt2map/application/generators/llm_map_generator.py
+++ prompt2map/application/generators/llm_map_generator.py
@@ -1,45 +1,55 @@
 import logging
 from typing import Any, Callable, Optional
+import folium
 import geopandas as gpd
 
-from prompt2map.application.maps.choropleth_map import choropleth_map
 from prompt2map.interfaces.core.map_generator import MapGenerator
 from prompt2map.interfaces.nlp.llm import LLM
 from prompt2map.types import Map
 
-def get_available_tools(data: gpd.GeoDataFrame) -> list[dict[str, Any]]:
+def get_available_tools(data: gpd.GeoDataFrame) -> list[dict[str, str | dict]]:
     return [
         {
             "type": "function",
             "function": {
-                "name": "create_choropleth_map",
-                "description": "Create a choropleth map",
+                "name": "create_colored_map",
+                "description": "Create a map with the selected column colored",
                 "parameters": {
                     "type": "object",
                     "properties": {
-                        "title": {
+                        "color_column": {
                             "type": "string",
-                            "description": "The title of the map",
-                        },
-                        "value_column": {
-                            "type": "string",
-                            "enum": list(data.select_dtypes(include='number').columns),
-                            "description": "The column to use for the choropleth map",
+                            "enum": list(data.select_dtypes(include="number").columns),
+                            "description": "This columns represents the variable that answers the user question. It will be used to color the map",
                         }
-                        
                     },
-                    "required": ["title", "value_column"],
+                    "required": ["color_column"],
                 },
             }
         },
     ]
 
 
-def create_choropleth_map(data: gpd.GeoDataFrame, title: str, value_column: str) -> Map:
-    return choropleth_map(data, value_column, title, "folium")
+def colored_map(data: gpd.GeoDataFrame, value_column: str, is_categorical: bool = False) -> folium.Map:
+    cmap = 'viridis'
+    if is_categorical:
+        data[value_column] = data[value_column].astype("category")
+        possible_values = len(data[value_column].value_counts(dropna=False))
+        if possible_values <= 10:
+            cmap = 'tab10'
+        elif possible_values <= 20:
+            cmap = 'tab20'
+        else:
+            data[value_column] = data[value_column].apply(lambda x: x if x in data[value_column].value_counts().nlargest(19).index else 'Other')
+            cmap = 'tab20'
+        return data.explore(value_column, cmap=cmap)
+    return data.explore(value_column, cmap=cmap, scheme="NaturalBreaks")
+
+def create_colored_map(data: gpd.GeoDataFrame, color_column: str, is_categorical: bool = False) -> Map:
+    return colored_map(data, color_column, is_categorical)
 
 available_functions: dict[str, Callable[..., Map]] = {
-    "create_choropleth_map": create_choropleth_map,
+    "create_colored_map": create_colored_map,
     # "create_bar_chart_map": create_bar_chart_map,   
 }
 
@@ -54,7 +64,14 @@ class LLMMapGenerator(MapGenerator):
         self.llm = llm
         self.tools = tools
         self.functions = functions
+        self.function_args: Optional[dict] = None
+        self.function_response: Optional[Map] = None
     
     def generate(self, prompt: str, data: gpd.GeoDataFrame) -> Optional[Map]:
         prompt = f"Create a map that answer the following question: {prompt}"
-        return self.llm.function_calling(prompt, system_prompt=None, functions=self.functions, tools=self.tools(data), data=data)
+        result = self.llm.function_calling(prompt, system_prompt=None, functions=self.functions, tools=self.tools(data), data=data)
+        if result is not None:
+            self.function_response, self.function_args = result
+            return self.function_response
+        return None
+        
--- prompt2map/interfaces/nlp/llm.py
+++ prompt2map/interfaces/nlp/llm.py
@@ -7,5 +7,6 @@ class LLM(Protocol):
     def chat(self, user_prompt: str, system_prompt: str) -> str:
         ...
         
-    def function_calling(self, user_prompt: Optional[str], system_prompt: Optional[str], functions: dict[str, Callable[..., T]], tools: list[dict], tool_choice: str = "auto", **kwargs) -> Optional[T]:
+    def function_calling(self, user_prompt: Optional[str], system_prompt: Optional[str], 
+                         functions: dict[str, Callable[..., T]], tools: list[dict], tool_choice: str = "auto", **kwargs) -> Optional[tuple[T, dict]]:
         ...
